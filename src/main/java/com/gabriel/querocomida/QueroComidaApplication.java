dcc80eae457b3ff51399ae5b5fb9f95b40c7da4f
brandaogabriel <gabriel.brandao@ccc.ufcg.edu.br>
2021-01-27 20:44:43 -0300

    chore: project created

---
+++ src/main/java/com/gabriel/querocomida/QueroComidaApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.querocomida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueroComidaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueroComidaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabriel/querocomida/QueroComidaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.querocomida;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueroComidaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
