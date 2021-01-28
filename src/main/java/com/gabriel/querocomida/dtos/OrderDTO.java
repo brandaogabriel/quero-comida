2fbf9134d04f9bdbf1cceb8b483c9d4756e98437
brandaogabriel <gabriel.brandao@ccc.ufcg.edu.br>
2021-01-28 15:24:05 -0300

    feat: add OrderStatus enum

--- src/main/java/com/gabriel/querocomida/dtos/OrderDTO.java
+++ src/main/java/com/gabriel/querocomida/dtos/OrderDTO.java
@@ -10,20 +10,23 @@ public class OrderDTO implements Serializable {
 
 	private Long id;
 	private Instant moment;
+	private Integer orderStatus;
 	private UserDTO client;
 
 	public OrderDTO() {
 	}
 
-	public OrderDTO(Long id, Instant moment, UserDTO client) {
+	public OrderDTO(Long id, Instant moment, Integer orderStatus, UserDTO client) {
 		this.id = id;
 		this.moment = moment;
+		this.orderStatus = orderStatus;
 		this.client = client;
 	}
 
 	public OrderDTO(Order entity) {
 		this.id = entity.getId();
 		this.moment = entity.getMoment();
+		this.orderStatus = entity.getOrderStatus().getCode();
 		this.client = new UserDTO(entity.getClient());
 	}
 
@@ -41,6 +44,14 @@ public class OrderDTO implements Serializable {
 
 	public void setMoment(Instant moment) {
 		this.moment = moment;
+	}
+
+	public Integer getOrderStatus() {
+		return orderStatus;
+	}
+
+	public void setOrderStatus(Integer orderStatus) {
+		this.orderStatus = orderStatus;
 	}
 
 	public UserDTO getClient() {
--- src/main/java/com/gabriel/querocomida/entities/Order.java
+++ src/main/java/com/gabriel/querocomida/entities/Order.java
@@ -1,5 +1,7 @@
 package com.gabriel.querocomida.entities;
 
+import com.gabriel.querocomida.entities.enums.OrderStatus;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.Instant;
@@ -13,7 +15,10 @@ public class Order  implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
 	private Instant moment;
+
+	private Integer orderStatus;
 
 	@ManyToOne
 	@JoinColumn(name = "client_id")
@@ -22,9 +27,10 @@ public class Order  implements Serializable {
 	public Order() {
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -38,6 +44,15 @@ public class Order  implements Serializable {
 
 	public void setMoment(Instant moment) {
 		this.moment = moment;
+	}
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null)
+			this.orderStatus = orderStatus.getCode();
 	}
 
 	public User getClient() {
