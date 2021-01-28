16465b619a46154d7e98611d5c023bf466fcc2f4
brandaogabriel <gabriel.brandao@ccc.ufcg.edu.br>
2021-01-28 11:02:48 -0300

    feat: add Order entity and relationships

--- src/main/java/com/gabriel/querocomida/entities/User.java
+++ src/main/java/com/gabriel/querocomida/entities/User.java
@@ -2,12 +2,13 @@ package com.gabriel.querocomida.entities;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_user")
 public class User implements Serializable {
-
 	private static final long serialVersionUID = 1L;
 
 	@Id
@@ -17,6 +18,9 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+
+	@OneToMany(mappedBy = "client")
+	private Set<Order> orders = new HashSet<>();
 
 	public User() {
 	}
@@ -63,6 +67,10 @@ public class User implements Serializable {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public Set<Order> getOrders() {
+		return orders;
 	}
 
 	@Override
